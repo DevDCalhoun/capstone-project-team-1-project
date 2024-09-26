0e10d8b75fccc9552c01a59319e3baf1dafb1847
Dcalhoun <Dcalhoun102193@gmail.com>
2024-09-26 15:51:33 -0500

    2nd attempt to fix automated test issues

--- tests/integration/routes.test.js
+++ tests/integration/routes.test.js
@@ -7,25 +7,10 @@ let expect;
   expect = chai.expect;
 })();
 
-before(async function () {
-  if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(process.env.DB_URL || 'mongodb://localhost:27017/disruptutorDB', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-  }
-});
-
 describe('App Routes', () => {
-this.timeout(10000);
-
   it('should render the home page', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).to.equal(200);
     expect(res.text).to.include('<title>Home Page</title>');
   });
 })
-
-after(async function () {
-  await mongoose.disconnect();
-});
