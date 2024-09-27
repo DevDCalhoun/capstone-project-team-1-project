f09334e17e7eb2dbe29f5c753869026375a7e25c
Dcalhoun <Dcalhoun102193@gmail.com>
2024-09-27 17:14:35 -0500

    Added new UI components and clarified comments and code sections

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,9 +55,9 @@ exports.postLogin = async (req, res, next) => {
 }
 
 exports.postLogout = (req, res) => {
-  req.session.destroy((err) => {
+  req.session.destroy((error) => {
     if (err) {
-      console.error("There was an error logging you out: ", err);
+      console.error("There was an error logging you out: ", error);
       return res.status(500).send('Internal Server Error');
     }
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const User = require('../models/user');
 const isAuthenticated = require('../middleware/auth');
 const userController = require('../controllers/userController');
 
