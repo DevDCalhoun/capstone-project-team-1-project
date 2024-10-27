7be5086b40a276a9d1f5ff3a44cb4cc641c67a3a
Low-Elo <jb306@students.uwf.edu>
2024-10-26 19:54:21 -0500

    Added createAppointment method and logging for new appointment

--- classes/appointmentManager.js
+++ classes/appointmentManager.js
@@ -1,8 +1,35 @@
 const Appointment = require('./appointment');
+const User = require('../models/appointment');
+const logger = require('../logging/logger');
 
 class AppointmentManager {
     // Constructor to initialize the appointment manager with userID(SessionID)
     constructor(userId) {
         this.userId = userId;
+    }
+
+    // Method creates a new appointment
+    async createAppointment(studentId, tutorId, day, time, details) {
+        try {
+            const newAppointment = new Appointment({
+                studentId,
+                tutorId,
+                day,
+                time,
+                details
+            });
+            // Save the appointment to the database
+            await newAppointment.save();
+
+            // Log the new appointment
+            if (newAppointment) {
+                logger.notice("New tutoring appointment created.");
+            }
+
+            return newAppointment;
+        } catch (error) {
+            console.error("Error creating appointment: ", error.message);
+            throw error;
+        }
     }
 }
