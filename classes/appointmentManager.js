553237c4e02d8b041edbd105020266b46d4a5621
Low-Elo <jb306@students.uwf.edu>
2024-10-26 19:36:27 -0500

    Added constructor to appointment class

--- classes/appointmentManager.js
+++ classes/appointmentManager.js
@@ -1,5 +1,8 @@
 const Appointment = require('./appointment');
 
 class AppointmentManager {
-    
+    // Constructor to initialize the appointment manager with userID(SessionID)
+    constructor(userId) {
+        this.userId = userId;
+    }
 }
