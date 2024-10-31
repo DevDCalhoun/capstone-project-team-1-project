8a67efe0e03994322f74527af13e1e017d9b4c39
Low-Elo <jb306@students.uwf.edu>
2024-10-31 11:13:06 -0500

    Added middleware file to authorize user roles

---
+++ middleware/authRole.js
@@ -0,0 +1,16 @@
+module.exports = function authorizeRole(...allowedRoles) {
+    return (req, res, next) => {
+        if (!req.session.userId) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        const userRole = req.session.userRole;  // Assuming the role is stored in session
+        if (!allowedRoles.includes(userRole)) {
+            return res.status(403).send('Forbidden: You do not have access to this resource');
+        }
+
+        next();
+    };
+};
+
+module.exports = authorizeRole;
